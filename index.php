f91dbab0a7b1e7028b80a683740d8dd8cead191e
aliidriss <57557411+aliidriss@users.noreply.github.com>
2019-11-09 17:04:23 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello World from Ali Idriss to all the word test2222222</p>'; ?> 
+ <?php echo '<p>Hello Word from Ali Idriss to all the world test2222222</p>'; ?> 
  </body>
 </html>

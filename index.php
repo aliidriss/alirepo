688ab7fda555e8c392bf8d5b0df318f096015a4a
aliidriss <57557411+aliidriss@users.noreply.github.com>
2019-11-09 13:44:44 +0200

    v2

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello World from Ali Idriss to all the word</p>'; ?> 
+ <?php echo '<p>Hello World from Ali Idriss to all the word test2222222</p>'; ?> 
  </body>
 </html>
